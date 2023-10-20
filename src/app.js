d7dd132f6054cb409f76d392657bd40fc3e90d39
megapubli <86963814+megapubli@users.noreply.github.com>
2023-10-20 11:36:26 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,34 +1,26 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const app = express()
-const httpolyglot = require('httpolyglot')
-const https = require('https')
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http'); // Usamos http em vez de https
+const port = process.env.PORT || 1000;
 
 //////// CONFIGURATION ///////////
 
-// insert your own ssl certificate and keys
-const options = {
-    key: fs.readFileSync(path.join(__dirname,'..','ssl','key.pem'), 'utf-8'),
-    cert: fs.readFileSync(path.join(__dirname,'..','ssl','cert.pem'), 'utf-8')
-}
-
-const port = process.env.PORT || 1000
+// Se você ainda tiver as chaves SSL, pode mantê-las aqui, mas não é necessário para um ambiente local.
+// const options = {
+//     key: fs.readFileSync(path.join(__dirname, '..', 'ssl', 'key.pem'), 'utf-8'),
+//     cert: fs.readFileSync(path.join(__dirname, '..', 'ssl', 'cert.pem'), 'utf-8')
+// };
 
 ////////////////////////////
 
-require('./routes')(app)
+require('./routes')(app);
 
-const httpsServer = https.createServer(app)
-const io = require('socket.io')(httpsServer)
-require('./socketController')(io)
+const httpServer = http.createServer(app); // Usamos http.createServer em vez de https.createServer
+const io = require('socket.io')(httpServer);
+require('./socketController')(io);
 
-
-httpsServer.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
-
-
-
-
-
+httpServer.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
